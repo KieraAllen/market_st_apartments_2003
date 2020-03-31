3e9b4059a3f3bd1dba293d3b4e9d0fa2eefcd3df
Kiera Allen <kieraaallen@gmail.com>
2020-03-31 10:56:08 -0600

    Work on adding renters to units and checking building for renter names

--- lib/building.rb
+++ lib/building.rb
@@ -1,11 +1,22 @@
 class Building
-  attr_reader :units
+  attr_reader :units,
+              :renters
 
   def initialize
     @units = []
+    @renters = []
   end
 
   def add_unit(unit)
     @units.push(unit)
+  end
+
+  def renters
+    unit.renter = units.map do |unit|
+      # unit.renter == [renter]
+      renters.push(renter)
+      end
+      @renters
+    end
   end
 end
--- test/building_test.rb
+++ test/building_test.rb
@@ -28,4 +28,22 @@ class BuildingTest < Minitest::Test
 
     assert_equal [unit1, unit2], building.units
   end
+
+  def test_it_starts_with_no_renters
+    building = Building.new
+
+    assert_equal [], building.renters
+  end
+
+  def test_it_has_renters
+    building = Building.new
+    unit1 = Apartment.new({number: "A1", monthly_rent: 1200, bathrooms: 1, bedrooms: 1})
+    renter1 = Renter.new("Aurora")
+
+    unit1.add_renter(renter1)
+    building.add_unit(unit1)
+    
+    assert_equal ["Aurora"], building.renters
+  end
+
 end
